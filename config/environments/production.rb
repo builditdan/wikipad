06f6306f810235e79c80c865d2a067a4d9aa6dbd
builditdan <daniels111@gmail.com>
2016-01-19 10:30:59 -0500

    Adjustments to the config files for mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-<<<<<<< HEAD
-  config.action_mailer.smtp_settings = {
+  cconfig.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
       domain: "localhost:3000", #Rails.application.secrets.domain_name,
@@ -98,12 +97,4 @@ Rails.application.configure do
   #config.action_mailer.smtp_settings = {:address => "localhost", :port => "1025"}
   #config.action_mailer.raise_delivery_errors = true
 
-=======
-  ActionMail config
-  config.action_mailer.default_url_options = { host: 'localhost', port:3000}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
->>>>>>> user_story_10_Wiki_Collaborators
 end
